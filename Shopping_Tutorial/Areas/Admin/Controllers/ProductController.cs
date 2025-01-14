c0702726b3b3834c33edb76fc0b39c019630058d
Duc Hieu 2412 <nguyenHoDucHieu2412@gmail.com>
2025-01-14 21:25:08 +0700

    Add files via upload

--- Shopping_Tutorial/Areas/Admin/Controllers/ProductController.cs
+++ Shopping_Tutorial/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -8,6 +9,8 @@ using Shopping_Tutorial.Repository;
 namespace Shopping_Tutorial.Areas.Admin.Controllers
 {
 	[Area("Admin")]
+	
+
 	
 	public class ProductController : Controller
 	{
@@ -86,7 +89,7 @@ namespace Shopping_Tutorial.Areas.Admin.Controllers
 		}
 
 
-		[Route("Edit")]
+		
 		public async Task<IActionResult> Edit(int id)
 		{
 			ProductModel product = await _dataContext.Products.FindAsync(id);
@@ -95,66 +98,39 @@ namespace Shopping_Tutorial.Areas.Admin.Controllers
 
 			return View(product);
 		}
-		[Route("Edit")]
+		
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(ProductModel product)
+		public async Task<IActionResult> Edit(int Id,ProductModel product)
 		{
 			ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
 			ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
-
-
-			var existed_product= _dataContext.Products.Find(product.Id);
-
 			if (ModelState.IsValid)
 			{
 				product.Slug = product.Name.Replace(" ", "-");
-				
+				var slug = await _dataContext.Products.FirstOrDefaultAsync(p => p.Slug == product.Slug);
+				if (slug != null)
+				{
+					ModelState.AddModelError("", "Sản phẩm đã có trong database");
+					return View(product);
+				}
 
 
 				if (product.ImageUpload != null)
 				{
-
 					string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
 					string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
 					string filePath = Path.Combine(uploadsDir, imageName);
-					//delete anh cu
-					string oldfilePath = Path.Combine(uploadsDir, existed_product.Image);
-					try
-					{
-
-						if (System.IO.File.Exists(oldfilePath))
-						{
-							System.IO.File.Delete(oldfilePath);
-						}
-					}
-					catch (Exception ex)
-					{
-						ModelState.AddModelError("", "An errer occurred while delething the product iname.");
-					}
 
 					FileStream fs = new FileStream(filePath, FileMode.Create);
 					await product.ImageUpload.CopyToAsync(fs);
 					fs.Close();
-					existed_product.Image = imageName;
-
-					
+					product.Image = imageName;
 				}
 
-				
-				existed_product.Description = product.Description;
-				existed_product.Name = product.Name;
-				existed_product.Price = product.Price;
-				existed_product.Slug = product.Slug;
-				existed_product.CategoryId = product.CategoryId;
-				existed_product.BrandId = product.BrandId;
-
-
-
-				_dataContext.Update(existed_product);
-
+				_dataContext.Update(product);
 				await _dataContext.SaveChangesAsync();
-				TempData["success"] = "Cập nhật sản phẩm thành công";
+				TempData["success"] = "Cập nhật phẩm thành công";
 				return RedirectToAction("Index");
 
 			}
@@ -174,31 +150,13 @@ namespace Shopping_Tutorial.Areas.Admin.Controllers
 				return BadRequest(errorMessage);
 
 			}
-			
-
+			return View(product);
 		}
-		public async Task<IActionResult> Delete(int id)
+		public async Task<IActionResult> Delete(int Id)
 		{
-			ProductModel product = await _dataContext.Products.FindAsync(id);
-			if (product == null)
-			{
-				return NotFound();
-			}
-			string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
-			//delete anh cu
-				string oldfileImage = Path.Combine(uploadsDir, product.Image);
-			try
-			{
-
-				if (System.IO.File.Exists(oldfileImage))
-				{
-					System.IO.File.Delete(oldfileImage);
-				}
-			}
-			catch (Exception ex)
-			{
-				ModelState.AddModelError("", "An errer occurred while delething the product iname.");
-			}
+			ProductModel product = await _dataContext.Products.FindAsync(Id);
+			
+			
 			//Cap nhat anh moi
 
 			
---
+++ Shopping_Tutorial/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Repository;
+
+namespace Shopping_Tutorial.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Route("Admin/User")]
+    [Authorize(Roles ="Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _rolerManager;
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext context,UserManager<AppUserModel> userManager, RoleManager<IdentityRole> rolerManager)
+        {
+            
+            _userManager = userManager;
+            _rolerManager = rolerManager;
+            _dataContext = context;
+        }
+        [HttpGet]
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var usersWithRoles = await (from u in _dataContext.Users
+                                        join ur in _dataContext.UserRoles on u.Id equals ur.UserId
+                                        join r in _dataContext.Roles on ur.RoleId equals r.Id
+                                        select new { User = u, RoleName = r.Name })
+                                 .ToListAsync();
+
+            return View(usersWithRoles);
+        }
+
+
+        [HttpGet]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _rolerManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles,"Id","Name");
+            return View(new AppUserModel());
+        }
+
+		[HttpGet]
+		[Route("Edit")]
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (string.IsNullOrEmpty(id))
+			{
+				return NotFound();
+			}
+			var user = await _userManager.FindByIdAsync(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			var roles = await _rolerManager.Roles.ToListAsync();
+			ViewBag.Roles = new SelectList(roles, "Id", "Name");
+			return View(user);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[Route("Edit")]
+		public async Task<IActionResult> Edit(string id, AppUserModel user)
+		{
+            var existingUser = await _userManager.FindByIdAsync(id);
+			if (existingUser == null)
+			{
+				return NotFound();
+			}
+			
+			if (ModelState.IsValid)
+			{
+				existingUser.UserName= user.UserName;
+                existingUser.Email = user.Email;
+                existingUser.PhoneNumber = user.PhoneNumber;
+                existingUser.RoleId = user.RoleId;
+
+                var updateUserResult = await _userManager.UpdateAsync(existingUser); //thuc hien update
+                if (updateUserResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    AddIdentityErrors(updateUserResult);
+                    return View(existingUser);
+                }
+			}
+           
+			var roles = await _rolerManager.Roles.ToListAsync();
+			ViewBag.Roles = new SelectList(roles, "Id", "Name");
+
+            TempData["error"] = "Model validation faild.";
+            var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e=>e.ErrorMessage)).ToList();
+            string erroeMessage = string.Join("\n",errors);
+
+			return View(existingUser);
+		}
+        
+		
+
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[Route("Create")]
+		public async Task<IActionResult> Create(AppUserModel user)
+		{
+			if (ModelState.IsValid) 
+            {
+            var createUserResult = await _userManager.CreateAsync(user,user.PasswordHash);
+                if (createUserResult.Succeeded)
+                {
+                    var createUser = await _userManager.FindByEmailAsync(user.Email); //tim user dua vao email
+                    var userId = createUser.Id;
+                    var role = _rolerManager.FindByIdAsync(user.RoleId);
+                    // gan quyen
+                    var addToRoleResult = await _userManager.AddToRoleAsync(createUser, role.Result.Name);
+                    if (!addToRoleResult.Succeeded)
+                    {
+                        foreach(var error in createUserResult.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                    return RedirectToAction("Index","User");
+                }
+                else
+                {
+					AddIdentityErrors(createUserResult);
+                    return View(user);
+                }
+            }
+            else {
+				TempData["error"] = "Model có một vài thứ dang bị lỗi";
+				List<string> errors = new List<string>();
+
+				foreach (var value in ModelState.Values)
+				{
+					foreach (var error in value.Errors)
+					{
+						errors.Add(error.ErrorMessage);
+					}
+				}
+				string errorMessage = string.Join("\n", errors);
+				return BadRequest(errorMessage);
+			}
+			var roles = await _rolerManager.Roles.ToListAsync();
+			ViewBag.Roles = new SelectList(roles, "Id", "Name");
+			return View(user);
+		}
+
+		private void AddIdentityErrors(IdentityResult identityResult)
+		{
+			foreach(var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+		}
+
+		[HttpGet]
+		[Route("Delete")]
+		public async Task<IActionResult> Delete(string id)
+		{
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+            var user = await _userManager.FindByIdAsync(id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            var deleteRelsult = await _userManager.DeleteAsync(user);
+            if (!deleteRelsult.Succeeded)
+            {
+                return View("Error");
+            }
+			TempData["serror"] = "User đã xóa";
+			return RedirectToAction("Index");
+			
+		}
+
+	}
+}
